a5dc1f74ae0a44fbfb07186ac1408c61414149e1
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-11-17 10:47:17 -0700

    Added File Reader

--- Playslist_Simulator/src/App.java
+++ Playslist_Simulator/src/App.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            Scanner scanner = new Scanner(new File("songs_list.csv"));
+            scanner.useDelimiter(",");
+            while (scanner.hasNext()) {
+                System.out.print(scanner.next() + " ");
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
     }
+
+    /*
+    public ArrayList<Song> readFile(String filePath) throws FileNotFoundException {
+        ArrayList<Song> songs = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(filePath));
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()) {
+            songs.add(scanner.next());
+        }
+        scanner.close();
+        return data;
+    }
+         */
 }
