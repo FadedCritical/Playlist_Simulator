4b826ae5c15c36d1377c890e1f1e37117642ef09
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-11-19 15:34:31 -0700

    updated

--- Playslist_Simulator/src/App.java
+++ Playslist_Simulator/src/App.java
@@ -5,37 +5,31 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<Song> songs;
+        ArrayList<Song> songs = getSongs("songs.csv");
+
+    }
+
+    public static ArrayList<Song> getSongs(String filePath) throws FileNotFoundException {
         try {
-            songs = getSongs("Playslist_Simulator\\songs_list.csv");
-            for (Song song : songs) {
-                System.out.println(song.toString());
+            ArrayList<Song> songs = new ArrayList<>();
+            Scanner scanner = new Scanner(new File(filePath));
+            scanner.useDelimiter(",");
+            scanner.nextLine(); // Skip header line
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] attributes = line.split(",");
+                String songName = attributes[0];
+                int durationInSeconds = Integer.parseInt(attributes[1]);
+                String artistName = attributes[2];
+                String genre = attributes[3];
+                Song song = new Song(songName, durationInSeconds, artistName, genre);
+                songs.add(song);
             }
+            scanner.close();
+            return songs;
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
-
-        
+        return null;
     }
-
-    
-    public static  ArrayList<Song> getSongs(String filePath) throws FileNotFoundException {
-        ArrayList<Song> songs = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(filePath));
-        scanner.useDelimiter(",");
-        scanner.nextLine(); // Skip header line
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] attributes = line.split(",");
-            String songName = attributes[0];
-            int durationInSeconds = Integer.parseInt(attributes[1]);
-            String artistName = attributes[2];
-            String genre = attributes[3];
-            Song song = new Song(songName, durationInSeconds, artistName, genre);
-            songs.add(song);
-        }
-        scanner.close();
-        return songs;
-    }
-    
 }
