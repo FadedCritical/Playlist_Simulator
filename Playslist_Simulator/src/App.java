fa86a9c884d6dd85c7464178f786e508ab204e8e
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-11-19 14:54:16 -0700

    Updated scanner

--- Playslist_Simulator/src/App.java
+++ Playslist_Simulator/src/App.java
@@ -1,31 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            Scanner scanner = new Scanner(new File("songs_list.csv"));
-            scanner.useDelimiter(",");
-            while (scanner.hasNext()) {
-                System.out.print(scanner.next() + " ");
-            }
-            scanner.close();
+            ArrayList<Song> songs = new ArrayList<>();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
     }
 
-    /*
-    public ArrayList<Song> readFile(String filePath) throws FileNotFoundException {
+    
+    public ArrayList<Song> getSongs(String filePath) throws FileNotFoundException {
         ArrayList<Song> songs = new ArrayList<>();
         Scanner scanner = new Scanner(new File(filePath));
         scanner.useDelimiter(",");
+        scanner.nextLine(); // Skip header line
         while (scanner.hasNext()) {
-            songs.add(scanner.next());
+            songs.add(new Song(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt()));
         }
         scanner.close();
-        return data;
+        return songs;
     }
-         */
+    
 }
