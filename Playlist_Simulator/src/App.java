b067728747040cecfd69c24baa06b623bf22991b
FadedCritical <ronald.armisteadtyler@gmail.com>
2025-12-01 16:06:32 -0700

    Added a loop for the songs

--- Playlist_Simulator/src/App.java
+++ Playlist_Simulator/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         PlaylistManager playlistManager = new PlaylistManager();
+
+        //gets the first 5 songs and prints their details
+        for (int i = 0; i < 5; i++) {
+            playlistManager.getSong(i).forEach(song -> System.out.println(song));
+        }
     }
 }
